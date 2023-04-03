ef9ffa0ad628b27a36af4bb6a5ddea78b23829cc
Tea-ho <hokeng92@naver.com>
2023-04-03 10:47:46 +0900

    23.04.03, 10:47 update

--- JSPWEB/src/main/webapp/practice/과제/과제04/js/modal.js
+++
@@ -1,9 +0,0 @@
-// console.log('연결확인');
-
-function openModal(){
-	document.querySelector('.modal_wrap').style.display = 'flex';
-}
-
-function closeModal(){
-	document.querySelector('.modal_wrap').style.display = 'none';
-}
